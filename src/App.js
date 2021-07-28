fb3ba50330ccd1e51e0bd62d6b6d3f05b40ec5db
Sophia <sophiansl2018@yahoo.com>
2021-07-28 10:43:24 +1000

    initial commit: cards line-chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
+import { fetchData } from './api';
+// import Image from './covid-19.png';
+// import Icon from './icon.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import {Cards, Chart, CountryPicker} from './components';
+
+class App extends React.Component {
+
+    state = {
+        data: {},
+    };
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({data: fetchedData}); 
+    }
+
+    render() {
+
+        return (
+            <div className={styles.container}>
+                {/* <img className = {styles.title} src = {Image} /> */}
+                <Cards cardData = {this.state.data}/>
+                {/* <img className= {styles.icon} src = {Icon} /> */}
+                <CountryPicker />
+                <Chart />
+            </div>
+        )
+    }
 }
 
 export default App;
